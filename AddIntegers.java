b9e59a698c8f5e384918ef021e936bb397213dcf
Melissa Paul <paulmeli@umich.edu>
2021-03-09 12:31:34 -0500

    Changed first 20 to 30

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,7 +2,7 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 10;
+        int first = 30;
         int second = 30;
 
         System.out.println("Enter two numbers: " + first + " " + second);
